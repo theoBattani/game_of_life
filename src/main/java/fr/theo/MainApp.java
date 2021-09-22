bb3cd121e062bad7d17ce427e97284c41ee331bc
theoBattani <84023706+theoBattani@users.noreply.github.com>
2021-09-22 09:17:13 +0200

    ...

--- src/main/java/fr/theo/MainApp.java
+++ src/main/java/fr/theo/MainApp.java
@@ -1,7 +1,6 @@
 package fr.theo;
 
 import javafx.application.Application;
-// import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
--- src/main/java/fr/theo/Settings.java
+++ src/main/java/fr/theo/Settings.java
@@ -4,18 +4,15 @@ import java.io.File;
 
 public class Settings {
 
-    private File file;
+    private static File file;
     
     private static int chunkSize;
+    private static double cellSize;
 
     private static double aspectRatio;
     private static boolean fullScreen;
     private static int frameRate;
 
-    private static void read(File file) {
-    }
-
-    private static void write(File file) {
-    
-    }
+    private static void read(File file) {}
+    private static void write(File file) {}
 }
